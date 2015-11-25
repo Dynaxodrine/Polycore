6c5c3d00bdeaa264795ee0b39d947faa18ab9422
BuildTools <unconfigured@null.spigotmc.org>
2015-11-25 12:11:05 +0100

    Initial commit

---
+++ Polycore/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Polycore.Startup))]
+namespace Polycore
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
