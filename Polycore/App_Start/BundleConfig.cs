b431f8e241544e10aff222add598207909c0becd
HugoC4 <hugokruis@hotmail.com>
2015-11-26 11:15:35 +0100

    Fixed script loading

--- Polycore/App_Start/BundleConfig.cs
+++ Polycore/App_Start/BundleConfig.cs
@@ -9,7 +9,38 @@ namespace Polycore
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/Bundles/scripts").Include(
-                        "~/Scripts/*.js"));
+                        "~/Scripts/jquery.js",
+                        "~/Scripts/jquery.cookie.pack.js",
+                        "~/Scripts/jquery-ui.js",
+                        "~/Scripts/jquery-migrate.min.js",
+                        "~/Scripts/jquery.fancybox.js",
+                        "~/Scripts/jquery.elastic.source.js",
+                        "~/Scripts/jquery.carouFredSel-6.2.1-packed.js",
+                        "~/Scripts/jquery-ui-1.10.3.custom.min.js",
+                        "~/Scripts/jquery.ui.totop.js",
+                        "~/Scripts/jquery.validate.min.js",
+                        "~/Scripts/login-with-ajax.js",
+                        "~/Scripts/bootstrap-button.js",
+                        "~/Scripts/bootstrap-carousel.js",
+                        "~/Scripts/bootstrap-collapse.js",
+                        "~/Scripts/bootstrap-modal.js",
+                        "~/Scripts/bootstrap-tab.js",
+                        "~/Scripts/bootstrap-tooltip.js",
+                        "~/Scripts/bootstrap-transition.js",
+                        "~/Scripts/bootstrap-popover.js",
+                        "~/Scripts/easing.js",
+                        "~/Scripts/global.js",
+                        "~/Scripts/imagescale.js",
+                        "~/Scripts/login-with-ajax.source.js",
+                        "~/Scripts/main.js",
+                        "~/Scripts/theme.min.js",
+                        "~/Scripts/tinymce.min.js",
+                        "~/Scripts/transit.js",
+                        "~/Scripts/admin.js",
+                        "~/Scripts/greensock.js",
+                        "~/Scripts/layerslider.transitions.js",
+                        "~/Scripts/layerslider.kreaturamedia.jquery.js",
+                        "~/Scripts/tabs.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                 "~/Content/style.css",
